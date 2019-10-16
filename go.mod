652d0988b2bf06f9f474fad7b49874693a2e7631
root <root@localhost.localdomain>
2019-10-16 15:35:19 -0500

    First

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bsdpunk/crime
+
+go 1.13
+
+require github.com/facebookincubator/ent v0.0.0-20191016113016-e75a817d63ed // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+//	"github.com/facebookincubator/ent"
+"github.com/bsdpunk/crime/ent"
+"github.com/facebookincubator/ent/dialect/sql"
+"log"
+)
+
+
+
+func main() {
+	client, err := sql.Open("mysql", "root:hxAWYa2YbW@tcp://127.0.0.1/crime_development")
+	if err != nil {
+		log.Fatalf("failed connecting to mysql: %v", err)
+	}
+	defer client.Close()
+	ctx := context.Background()
+	// Run migration.
+	err = client.Schema.Create(
+		ctx, 
+		migrate.WithDropIndex(false),
+		migrate.WithDropColumn(false), 
+	)
+
+	if err := client.Schema.Create(ctx); err != nil {
+		    log.Fatalf("failed creating schema resources: %v", err)
+	    }
+}
